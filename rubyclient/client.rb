bc70ab0cef632def85ce03e6e03a9c3afc734ac0
Alexey <github_job@mackevich.addymail.com>
2024-12-16 20:35:03 +0500

    Create ruby client for proto

--- rubyclient/client.rb
+++ rubyclient/client.rb
@@ -1,2 +1,14 @@
 require 'bundler/setup'
 Bundler.require(:default)
+
+require_relative 'cross_orm_services_pb'
+
+stub = Org::Example::Crossorm::CrossORM::Stub.new('localhost:8980', :this_channel_is_insecure)
+
+get_user = stub.get_user(Org::Example::Crossorm::UserID.new(id: 1))
+
+puts "Get user #{get_user}"
+
+create_user = stub.create_user(Org::Example::Crossorm::NewUser.new(name: "new user", age: 18))
+
+puts "Create user #{create_user}"
